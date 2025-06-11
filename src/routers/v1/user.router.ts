2f2fc0660d217d63f2e879060e6faa07a9cb5e05
siddharthpd <sidpd510@gmail.com>
2025-06-11 17:34:33 +0530

    feat: add validation for user sign-up and sign-in, and apply in validateRequestBody

--- src/routers/v1/user.router.ts
+++ src/routers/v1/user.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { getAllUsersHandler, signInHandler, signUpHandler } from "../../controllers/user.controller";
+import { validateRequetBody } from "../../validators";
+import { signInSchema, signUpSchema } from "../../validators/user.validator";
 
 const userRouter = express.Router();
 
-userRouter.post('/signup', signUpHandler);
-userRouter.post('/signin', signInHandler);
+userRouter.post('/signup', validateRequetBody(signUpSchema), signUpHandler);
+userRouter.post('/signin', validateRequetBody(signInSchema), signInHandler);
 userRouter.get('/', getAllUsersHandler);
 
 
---
+++ src/validators/user.validator.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+export const signUpSchema = z.object({
+    name: z.string({
+        required_error: "Name is required",
+    }).min(1, "Name should alleast have one character"),
+    email: z.string({
+        required_error: "Email is required",
+    }).email("Invalid email address"),
+    password: z.string({
+        required_error: "Password is required",
+    }).regex(strongPasswordRegex, "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+});
+
+
+export const signInSchema = z.object({
+    email: z.string({
+        required_error: "Email is required",
+    }).email("Invalid email address"),
+    password: z.string({
+        required_error: "Password is required",
+    }).regex(strongPasswordRegex, "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+});
