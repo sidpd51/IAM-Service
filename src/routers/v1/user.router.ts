6106b1495309e622e945ff0955dfd3cc8d430a39
siddharthpd <sidpd510@gmail.com>
2025-06-12 15:24:08 +0530

    feat: refactor user flow to assign "user" role on new user creation

--- src/routers/v1/user.router.ts
+++ src/routers/v1/user.router.ts
@@ -5,9 +5,9 @@ import { signInSchema, signUpSchema } from "../../validators/user.validator";
 
 const userRouter = express.Router();
 
+userRouter.get('/', getAllUsersHandler);
 userRouter.post('/signup', validateRequetBody(signUpSchema), signUpHandler);
 userRouter.post('/signin', validateRequetBody(signInSchema), signInHandler);
-userRouter.get('/', getAllUsersHandler);
 
 
 export default userRouter;
