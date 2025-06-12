f729ca7b34529fd7b3fd2a4f0cb59fd68f8182db
siddharthpd <sidpd510@gmail.com>
2025-06-12 12:57:35 +0530

    feat: update package.json scripts and add seed functionality with roles and users

---
+++ src/prisma/samples/user.ts
@@ -0,0 +1,52 @@
+export const users = [
+    {
+        email: "aarya@example.com",
+        password: "Arya@123",
+        name: "aarya",
+    },
+    {
+        email: "rohan@example.com",
+        password: "Rohan#89",
+        name: "rohan",
+    },
+    {
+        email: "tanya@example.com",
+        password: "Tanya$77",
+        name: "tanya",
+    },
+    {
+        email: "dev@example.com",
+        password: "Dev!2024",
+        name: "dev",
+    },
+    {
+        email: "isha@example.com",
+        password: "Isha@456",
+        name: "isha",
+    },
+    {
+        email: "kabir@example.com",
+        password: "Kabir#91",
+        name: "kabir",
+    },
+    {
+        email: "neha@example.com",
+        password: "Neha$321",
+        name: "neha",
+    },
+    {
+        email: "omkar@example.com",
+        password: "Omkar!55",
+        name: "omkar",
+    },
+    {
+        email: "sanya@example.com",
+        password: "Sanya@99",
+        name: "sanya",
+    },
+    {
+        email: "yash@example.com",
+        password: "Yash#100",
+        name: "yash",
+    },
+];
