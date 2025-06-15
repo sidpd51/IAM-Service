6e5198225cb69636453329935c1f07a8e126b2e0
sidpd51 <sidpd510@gmail.com>
2025-06-15 20:25:28 +0530

    refactor: reorganize user router to separate signup and signin handlers into index router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import userRouter from './user.router';
+import { validateRequetBody } from '../../validators';
+import { signInSchema, signUpSchema } from '../../validators/user.validator';
+import { signInHandler, signUpHandler } from '../../controllers/user.controller';
 
 const v1Router = express.Router();
 
+userRouter.post('/signup', validateRequetBody(signUpSchema), signUpHandler);
+userRouter.post('/signin', validateRequetBody(signInSchema), signInHandler);
 v1Router.use('/users', userRouter);
 
 export default v1Router;
--- src/routers/v1/user.router.ts
+++ src/routers/v1/user.router.ts
@@ -1,13 +1,11 @@
 import express from "express";
-import { getAllUsersHandler, signInHandler, signUpHandler, hasRoleHandler, AddRoleHandler } from "../../controllers/user.controller";
+import { AddRoleHandler, getAllUsersHandler, hasRoleHandler } from "../../controllers/user.controller";
 import { validateRequetBody } from "../../validators";
-import { AddRoleSchema, hasRoleSchema, signInSchema, signUpSchema } from "../../validators/user.validator";
+import { AddRoleSchema, hasRoleSchema } from "../../validators/user.validator";
 
 const userRouter = express.Router();
 
 userRouter.get('/', getAllUsersHandler);
-userRouter.post('/signup', validateRequetBody(signUpSchema), signUpHandler);
-userRouter.post('/signin', validateRequetBody(signInSchema), signInHandler);
 userRouter.post('/hasrole', validateRequetBody(hasRoleSchema), hasRoleHandler);
 userRouter.post('/addrole', validateRequetBody(AddRoleSchema), AddRoleHandler);
 
